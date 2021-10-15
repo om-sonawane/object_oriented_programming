c305c6064f14362bf374207c90f39819c5e277b5
OM SONAWANE <80897667+om-sonawane@users.noreply.github.com>
2021-10-15 00:27:31 -0700

    new

--- object_oriented_programming.py
+++ object_oriented_programming.py
@@ -1,1 +1,9 @@
- 
+ class Railwayform:
+    formType="Railwayform"
+    def printdata(self):
+        print(f"name is{self.name}")
+        print(f"train is{self.train}")
+    harrysapplication= Railwayform()
+    harrysapplication.name="harry"
+    harrysapplication.train="Rajdhani Express"
+    harrysapplication.printdata()
