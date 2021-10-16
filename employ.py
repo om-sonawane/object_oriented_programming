401132a677ccf2132434b998698e647f241ac6ff
OM SONAWANE <80897667+om-sonawane@users.noreply.github.com>
2021-10-16 05:35:28 -0700

    new

---
+++ employ.py
@@ -0,0 +1,12 @@
+class employee:
+    company ="Google"
+
+om = employee()
+randomgirl=employee()
+om.salary= 300
+randomgirl.salary=400
+print(om.company)
+print(randomgirl.company)
+employee.company="youtube"
+print(om.company)
+print(randomgirl.company)    
