013abaff9907ef1b1124b1bd6b476dfbf3e7bfd1
OM SONAWANE <80897667+om-sonawane@users.noreply.github.com>
2021-10-16 04:54:37 -0700

    game example

---
+++ game_example.py
@@ -0,0 +1,14 @@
+class player:
+    def moveRight(self):
+        pass
+   
+    def moveLeft(self):
+        pass
+   
+    def moveTop(self):
+        pass
+
+remote1= "remote"    
+player1 = player()
+if (remote1.pressed()):
+    player1.moveLeft()
