5033f72fc7c48a854ceaaf175e5b8f718844393f
OM SONAWANE <80897667+om-sonawane@users.noreply.github.com>
2021-10-19 02:49:45 -0700

    square_root_calculator

---
+++ claculator.py
@@ -0,0 +1,18 @@
+class calculator:
+   
+    def __init__(self,num):
+        self.number=num
+
+    def square(self):
+        print(f"the value of {self.number} square  is {self.number **2}")
+
+    def squareRoot(self):
+         print(f"the value of {self.number} square root  is {self.number **0.5}")
+    
+    def cube (self):
+         print(f"the value of {self.number} cube is {self.number **3}")
+
+a= calculator(9)
+a.square()
+a.squareRoot()
+a.cube()  
