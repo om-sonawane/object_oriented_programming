69bba9ffdd189aa1abd3cb15fdc106d568f7bcda
OM SONAWANE <80897667+om-sonawane@users.noreply.github.com>
2021-10-22 02:49:21 -0700

    commit_new

---
+++ inheritance_oops.py/property_decorator.py
@@ -0,0 +1,11 @@
+class   Employee:
+    company ="bharat gas"
+    salary=5600
+    salarybonus= 500
+    #totalsalary= 6100
+
+    def totalsalary(self):
+        return self.salary + self.salarybonus
+
+    e= ()
+    print(e.totalsalary)
