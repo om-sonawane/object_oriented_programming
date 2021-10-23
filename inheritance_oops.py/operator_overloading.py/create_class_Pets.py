693dfa67600f790661ea6a256b26ccd474bb6aa7
OM SONAWANE <80897667+om-sonawane@users.noreply.github.com>
2021-10-23 03:06:42 -0700

    new commit

---
+++ inheritance_oops.py/operator_overloading.py/create_class_Pets.py
@@ -0,0 +1,14 @@
+class Animals:
+    animaltype= "mammal"
+
+class pets:
+    colour="white"
+
+class dog:
+    @staticmethod
+    def bark(): 
+        print ("bow bow!!")   
+
+d = dog()
+d.bark()        
+
